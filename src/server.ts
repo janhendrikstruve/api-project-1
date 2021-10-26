eee1fe7b9f16172695152f2525f489bd40783b46
janhendrikstruve <janhendrikstruve@gmail.com>
2021-10-26 11:36:46 +0200

    Add rarams response

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,41 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
+app.get('/api/users/:name', (request, response) => {
+  response.send(request.params.name);
+});
+
 app.get('/', (_req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/api/users', (_request, response) => {
+  const users = ['Sven', 'Michael', 'Axel'];
+  response.send(users);
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+// import express from 'express';
+
+// const app = express();
+// const port = 3000;
+
+// app.get('/api/users/:name', (_request, response) => {
+//   response.send('Leon');
+// });
+
+// app.get('/api/users', (_request, response) => {
+//   const users = ['Manuel', 'Leon', 'Anke'];
+//   response.send(users);
+// });
+
+// app.get('/', (_req, res) => {
+//   res.send('Hello World 🐱‍👤!');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
